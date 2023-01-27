337b7dd5bad9e03253ae7973faafb8159c061a62
GedpPrince <Gedottt@gmail.com>
2023-01-28 00:02:42 +0300

    is just a comment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'green-color': '#69FF9B',
+      }
+    }
+  },
+  plugins: [],
+}
